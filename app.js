03fedfca7e0d72c505ba54a129d9541a0cd19f16
colinmatthews <matth11f@uwindsor.ca>
2017-01-28 00:38:47 -0500

    updated port

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://admin:admin@ds049935.mlab.com:49935/busdata'); // connect to our database
